d673c3bf0ba06a56a5b18e964e2a9abed2644b3e
Nelo Wallus <nelo@wallus.de>
2015-01-23 07:05:37 +0000

    Corrected unittests
    The first unittest were supposedly sufficient, but didn't
    check the request-method. This way is vastly clearer and
    more secure.

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -3,199 +3,272 @@
 import unittest
 from unittest.mock import call, patch
 import wrapper
+from json import dumps, loads
+import tempfile
+
+notebook_id = '1'
+notebook2_id = '2'
+new_notebook_id = '2'
+notebook_title = 'notebook title'
+notebook = {
+   'id': notebook_id,
+   'title': notebook_title
+   }
+notebook2 = {
+   'id': notebook2_id,
+   'title': notebook_title
+   }
+notebooks = [
+   notebook,
+   notebook2
+   ]
+
+version_id = '10'
+version2_id = '11'
+version = {
+        'id': version_id
+        }
+version2 = {
+        'id': version2_id
+        }
+versions = [
+        version,
+        version2
+        ]
+
+attachment_id = '55'
+attachment2_id = '56'
+attachment_file = 'attached.pdf'
+attachment = {
+        'id': attachment_id,
+        'file': attachment_file
+        }
+attachment2 = {
+        'id': attachment2_id,
+        'file': attachment_file
+        }
+attachments = [
+        attachment,
+        attachment2
+        ]
+
+tag_id = '42'
+tag2_id = '43'
+tag_title = 'some_tag'
+
+tag = {
+       'id': tag_id,
+       'title': tag_title,
+       'visibility': 0
+       }
+tag2 = {
+        'id': tag2_id,
+        'title': tag_title,
+        'visibility': 0
+        }
+tags = [
+        tag,
+        tag2
+        ]
+
+note_title = 'note title'
+content = 'some content'
+note_id = '4'
+note2_id = '5'
+
+note = {
+   'id': note_id,
+   'title': note_title,
+   'content': content,
+   'notebook_id': notebook_id,
+   'tags': [
+       tag
+       ],
+   'versions': [
+           version
+           ]
+        }
+note2 = {
+        'id': note2_id,
+        'title': note_title,
+        'content': content,
+        'notebook_id': notebook_id,
+        'tags': [
+            tag2
+            ],
+        'versions': [
+            version2
+            ]
+        }
+notes = [
+        note,
+        note2
+        ]
+
+keyword = 'test keyword'
+keyword_b64 = 'dGVzdCBrZXl3b3Jk'
+
+user = 'testuser'
+passwd = 'testpassword'
+upasswd = 'dGVzdHVzZXI6dGVzdHBhc3N3b3Jk'
+uri = 'test/uri'
+uri_correct = 'http://test/uri'
+agent = 'dummy agent'
+
+move = [{
+        'notebook': notebook,
+        'id': note_id,
+        'notebook_id': notebook_id,
+        'version_id': version_id
+        }]
+
+tagged = [
+        note
+        ]
+
+search = [
+        note2
+        ]
+
+i18n = {
+        'holy': 'moly',
+        'giant': 'dict'
+        }
+
+i18nkey = {
+        'giant': 'dict' #not so much
+        }
+
+ret = {
+        'notebooks':   notebooks,
+        'notebook':    notebook,
+        'notes':       notes,
+        'note':        note,
+        'move':        move,
+        'versions':    versions,
+        'version':     version,
+        'attachments': attachments,
+        'attachment':  attachment,
+        'tags':        tags,
+        'tag':         tag,
+        'tagged':      tagged,
+        'search':      search,
+        'i18n':        i18n,
+        'i18nkey':     i18n
+        }
 
 class TestRequests(unittest.TestCase):
     def setUp(self):
-        self.patcher = patch('wrapper.api.request')
-        self.mocked_request = self.patcher.start()
-
-        self.notebook_id = '1'
-        self.new_notebook_id = '2'
-        self.notebook_title = 'notebook title'
-        self.notebook = {
-                'id': self.notebook_id,
-                'title': self.notebook_title
-                }
-        self.note_title = 'note title'
-        self.content = 'some content'
-        self.note_id = '2'
-        self.version_id = '10'
-        self.attachment = ''
-        self.attachment_id = '55'
-        self.note = {
-                'id': self.note_id,
-                'title': self.note_title,
-                'content': self.content,
-                'notebook_id': self.notebook_id,
-                'versions':[
-                    { 'id': self.version_id }
-                    ]
-                }
-
-        self.tag_id = '34'
-        self.tag_title = 'some_tag'
-
-        self.tag = {
-                'id': self.tag_id,
-                'title': self.tag_title,
-                'visibility': 0
-                }
-
-        self.keyword = 'test keyword'
-        self.keyword_b64 = 'dGVzdCBrZXl3b3Jk'
-
-        self.user = 'testuser'
-        self.passwd = 'testpassword'
-        self.uri = 'test/uri'
-        self.uri_correct = 'http://test/uri'
-        self.agent = 'dummy agent'
-        self.api = wrapper.api(self.user, self.passwd, self.uri, self.agent)
+        self.api = wrapper.api(user, passwd, uri, agent)
+        self.patcher = patch('wrapper.urlopen')
+        self.mocked_urlopen = self.patcher.start()
 
     def tearDown(self):
         self.patcher.stop()
 
     def test_b64(self):
-        self.assertEqual(wrapper.b64(self.keyword), self.keyword_b64)
+        self.assertEqual(wrapper.b64(keyword), keyword_b64)
 
     def test_init(self):
-        self.assertEqual(self.api.host, self.uri_correct)
-        self.assertEqual(self.api.headers['User-Agent'], self.agent)
-        self.assertEqual(self.api.headers['Authorization'], 'Basic ' + wrapper.b64('{}:{}'.format(self.user, self.passwd)))
+        self.assertEqual(self.api.host, uri_correct)
+        self.assertEqual(self.api.headers['User-Agent'], agent)
+        self.assertEqual(self.api.headers['Authorization'], 'Basic ' + upasswd)
 
-    def asserts(self, response, *args):
-        self.assertTrue(self.mocked_request.called)
-        self.assertEqual(self.mocked_request.call_args_list, [call(*args)])
-        self.assertEqual(self.mocked_request.return_value, response)
+    def request(self, function, keyword, *args):
+        temp = tempfile.TemporaryFile()
+        temp.write(dumps(
+                {
+                'success': True,
+                'response': ret[keyword]
+                }
+            ).encode('ASCII'))
+        temp.seek(0)
+        self.mocked_urlopen.return_value = temp
 
-    def get_asserts(self, response, *args):
-        self.asserts(response, None, 'GET', *args)
+        response = function(*args)
 
-    def post_asserts(self, response, data, *args):
-        self.asserts(response, data, 'POST', *args)
+        self.mocked_urlopen.assert_called()
+        self.assertEqual(response, ret[keyword])
 
-    def put_asserts(self, response, data, *args):
-        self.asserts(response, data, 'PUT', *args)
-
-    def delete_asserts(self, response, *args):
-        self.asserts(response, None, 'DELETE', *args)
-
-    #actual tests
     def test_list_notebooks(self):
-        self.mocked_request.return_value = [ self.notebook ]
-        self.get_asserts(self.api.list_notebooks(),
-            'notebooks')
+        self.request(self.api.list_notebooks, 'notebooks')
 
     def test_create_notebook(self):
-        data = {
-                'type': 0,
-                'title': self.notebook_title,
-                'shortcut': ''
-                }
-        self.post_asserts(self.api.create_notebook(self.notebook_title),
-            data, 'notebooks')
+        self.request(self.api.create_notebook, 'notebooks', notebook_title)
 
     def test_get_notebook(self):
-        self.get_asserts(self.api.get_notebook(self.notebook_id),
-            'notebook', self.notebook_id)
+        self.request(self.api.get_notebook, 'notebook', notebook_id)
 
     def test_update_notebook(self):
-        response = self.api.update_notebook(self.notebook)
-        self.put_asserts(response, self.notebook, 'notebook', self.notebook_id)
+        self.request(self.api.update_notebook, 'notebook', notebook)
 
     def test_remove_notebook(self):
-        self.delete_asserts(self.api.remove_notebook(self.notebook_id),
-            'notebook', self.notebook_id)
+        self.request(self.api.remove_notebook, 'notebook', notebook_id)
 
     def test_list_notebook_notes(self):
-        self.get_asserts(self.api.list_notebook_notes(self.notebook_id),
-            'notes', self.notebook_id)
+        self.request(self.api.list_notebook_notes, 'notes', notebook_id)
 
     def test_create_note(self):
-        data = {'title': self.note_title, 'content': self.content}
-        self.post_asserts(self.api.create_note(self.notebook_id, self.note_title, self.content),
-            data, 'notes', self.notebook_id)
+        self.request(self.api.create_note, 'notes', notebook_id, note_title, content)
 
     def test_get_note(self):
-        self.get_asserts(self.api.get_note(self.notebook_id, self.note_id),
-            'note', self.notebook_id, self.note_id)
+        self.request(self.api.get_note, 'note', notebook_id, note_id)
 
     def test_get_notes(self):
-        self.get_asserts(self.api.get_notes(self.notebook_id, [self.note_id]),
-            'note', self.notebook_id, self.note_id)
+        self.request(self.api.get_notes, 'note', notebook_id, notes)
 
     def test_update_note(self):
-        self.put_asserts(self.api.update_note(self.note),
-            self.note, 'note', self.notebook_id, self.note_id)
+        self.request(self.api.update_note, 'note', note)
 
     def test_remove_note(self):
-        self.delete_asserts(self.api.remove_note(self.note),
-            'notes', self.notebook_id, self.note_id)
+        self.request(self.api.remove_note, 'notes', note)
 
     def test_remove_notes(self):
-        self.delete_asserts(self.api.remove_notes([self.note]),
-            'notes', self.notebook_id, self.note_id)
+        self.request(self.api.remove_notes, 'notes', notes)
 
     def test_move_note(self):
-        self.get_asserts(self.api.move_note(self.note, self.new_notebook_id),
-            'move', self.notebook_id, self.note_id, self.new_notebook_id)
+        self.request(self.api.move_note, 'move', note, new_notebook_id)
 
     def test_move_notes(self):
-        self.get_asserts(self.api.move_notes([self.note], self.new_notebook_id),
-            'move', self.notebook_id, self.note_id, self.new_notebook_id)
+        self.request(self.api.move_notes, 'move', notes, new_notebook_id)
 
     def test_list_note_versions(self):
-        self.get_asserts(self.api.list_note_versions(self.note),
-            'versions', self.notebook_id, self.note_id)
+        self.request(self.api.list_note_versions, 'versions', note)
 
     def test_list_notes_versions(self):
-        self.get_asserts(self.api.list_notes_versions([self.note]),
-            'versions', self.notebook_id, self.note_id)
+        self.request(self.api.list_notes_versions, 'versions', notes)
 
     def test_get_note_version(self):
-        self.get_asserts(self.api.get_note_version(self.note, self.version_id),
-            'version', self.notebook_id, self.note_id, self.version_id)
+        self.request(self.api.get_note_version, 'version', note, version_id)
 
     def test_list_note_attachments(self):
-        self.get_asserts(self.api.list_note_attachments(self.note),
-            'attachments', self.notebook_id, self.note_id, self.version_id)
+        self.request(self.api.list_note_attachments, 'attachments', note)
 
     def test_get_note_attachment(self):
-        self.get_asserts(self.api.get_note_attachment(self.note, self.attachment_id),
-            'attachment', self.notebook_id, self.note_id, self.version_id, self.attachment_id)
+        self.request(self.api.get_note_attachment, 'attachment', note, attachment_id)
 
     def test_remove_note_attachment(self):
-        self.delete_asserts(self.api.remove_note_attachment(self.note, self.attachment_id),
-            'attachment', self.notebook_id, self.note_id, self.version_id, self.attachment_id)
+        self.request(self.api.remove_note_attachment, 'attachment', note, attachment_id)
 
     # TODO (Nelo Wallus): Fix actual method
     @unittest.expectedFailure
     def test_upload_attachment(self):
-        self.post_asserts(self.api.upload_attachment(self.note, self.attachment),
-            self.attachment, 'attachments', self.notebook_id, self.note_id, self.version_id)
+        self.request(self.api.upload_attachment, 'attachments', note, attachment)
 
     def test_list_tags(self):
-        self.get_asserts(self.api.list_tags(),
-            'tags')
+        self.request(self.api.list_tags, 'tags')
 
     def test_get_tag(self):
-        self.get_asserts(self.api.get_tag(self.tag_id),
-            'tag', self.tag_id)
+        self.request(self.api.get_tag, 'tag', tag_id)
 
     def test_list_tagged(self):
-        self.get_asserts(self.api.list_tagged(self.tag_id),
-            'tagged', self.tag_id)
+        self.request(self.api.list_tagged, 'tagged', tag_id)
 
     def test_search(self):
-        self.get_asserts(self.api.search(self.keyword),
-            'search', wrapper.b64(self.keyword))
+        self.request(self.api.search, 'search', keyword)
 
     def test_i18n(self):
-        self.get_asserts(self.api.i18n(),
-            'i18n')
+        self.request(self.api.i18n, 'i18n')
 
     def test_i18n_param(self):
-        self.get_asserts(self.api.i18n(self.keyword),
-            'i18nkey', self.keyword)
-
-if __name__ == "__main__":
-    unittest.main()
+        self.request(self.api.i18n, 'i18nkey', keyword)
