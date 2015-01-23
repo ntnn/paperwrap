7d6753bd2f7b2c649bf3e15248f4b2fee5b72c8b
Nelo Wallus <nelo@wallus.de>
2015-01-23 14:47:27 +0000

    Only provide 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
