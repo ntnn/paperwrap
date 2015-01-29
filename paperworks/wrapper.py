b3a29c9f242cc57150ba703ca05277707d91b201
Nelo Wallus <nelo@wallus.de>
2015-01-29 01:13:40 +0000

    Updated version

--- paperworks/wrapper.py
+++ paperworks/wrapper.py
@@ -12,7 +12,7 @@ import json
 
 logger = logging.getLogger('paperwork-wrapper')
 
-__version__ = '0.11'
+__version__ = '0.12'
 api_version = '/api/v1/'
 user_agent = 'paperwork.py v{}'.format(__version__)
 
